4658642cff58b16675a7c776263b09413f9f93cd
Silvina Figueroa <silvina.sfigueroa@gmail.com>
2024-06-07 14:40:49 -0700

    views and user routes

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,63 @@
 const express = require('express') // importing express
 const router = express.Router() // creating a router app 
 
+// every single router will use the logger function
+router.use(logger)
+
 // routers for users 
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.send('User List')
 
 })
 
-router.get('/new', (req,res) => {
+router.get('/new', (req, res) => {
     res.send("User New Form")
-    
+
 })
+
+// use the colon to indicate dynamic parameter
+// anything that comes after the colon will become the parameter
+// router.get('/:id', (req, res) => {
+//     res.send(`User Get id ${req.params.id}`)
+// })
+
+// router.put('/:id', (req, res) => {
+//     res.send(`User Get id ${req.params.id}`)
+// })
+
+// router.delete('/:id', (req, res) => {
+//     res.send(`User Get id ${req.params.id}`)
+
+// })
+
+
+// This version nest all the methods for the same path, avoiding repetition
+
+router.route('/:id')
+    .get((req, res) => {
+        res.send(`User Get id ${req.params.id}`)
+        console.log(req.users)
+    }).put((req, res) => {
+        res.send(`User Get id ${req.params.id}`)
+    }).delete((req, res) => {
+        res.send(`User Get id ${req.params.id}`)
+    })
+
+
+const users = [{name : "Silvina"}, {name: "Alejandra"}]
+
+// this function will run everytime that finds a params that matchs the named passed
+router.param("id", (req, res, next, id) => {
+    req.users = users[id]
+    next() // If we don't add the function Next(), the code will continue running 
+    // we need to tell the programm to run the NEXT code
+})
+
+
+function logger(req, res, next){
+    console.log(req.originalUrl)
+    next()
+}
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')  // requiere the express library installed
 const app = express() // call express as a function to set up the server
 
+// name of the setting view engine, view engine: ejs
 app.set("view engine", "ejs")
 
-// create routes
-app.get("/",(req, res) => {  // these are names conventions to request and response, but you can change them
+// app.use(logger)
+
+// create routes - Multiples functions can be passed on the app.method
+app.get("/",logger, (req, res) => {  // these are names conventions to request and response, but you can change them
     console.log('Here')
     // res.send('Hi') // send is used mostly for testing
     // res.sendStatus(400) // this is displayed directly to the user with Internal Server Error (500)
@@ -12,14 +15,17 @@ app.get("/",(req, res) => {  // these are names conventions to request and respo
     // res.status(500).json({message :" Error"}) // Console error and a json back to the user - Useful for API's
     // res.download("server.js") // download a file!
     
-    res.render("index", { text: "myText" })
+    res.render("index", { text: "pulling text from server.js to index.html" })
 
 })
+
 
 
 const userRouter = require('./routes/users')
 
 // link the app with the router
 app.use("/users",userRouter) // pass path, router
+
+
 
 app.listen(3000) // add a port to the server for listening
