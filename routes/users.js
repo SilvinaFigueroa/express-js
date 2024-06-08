1e6b391cc6b6bdee02138b99446f192fc52014ae
Silvina Figueroa <silvina.sfigueroa@gmail.com>
2024-06-07 17:06:21 -0700

    user form / add user

--- routes/users.js
+++ routes/users.js
@@ -4,18 +4,36 @@ const router = express.Router() // creating a router app
 
 // every single router will use the logger function
 router.use(logger)
+let users = [{firstName : "Silvina"}, {firstName: "Alejandra"}]
 
 // routers for users 
 
 router.get('/', (req, res) => {
-    res.send('User List')
-
+    const name = req.query.name
+    res.send(`User in query param ${name}`)
 })
 
-router.get('/new', (req, res) => {
-    res.send("User New Form")
-
+// render the view users/new
+router.get("/new",(req,res)=> {
+    res.render("users/new")
 })
+
+
+router.post('/', (req, res) => {
+    const isValid = true
+    if(isValid){
+        users.push({firstName : req.body.firstName})
+        res.redirect(`users/${users.length - 1}`)
+    }
+    else {
+    console.log("Error")
+    res.render("users/new", {firstName : req.body.firstName})
+    }
+})
+
+// router.post("/",(req,res)=> {
+//     res.send("Create User")
+// })
 
 // use the colon to indicate dynamic parameter
 // anything that comes after the colon will become the parameter
@@ -37,23 +55,24 @@ router.get('/new', (req, res) => {
 
 router.route('/:id')
     .get((req, res) => {
-        res.send(`User Get id ${req.params.id}`)
+        const userId = req.params.id
+        console.log(userId)
+        res.send(`User Created id ${userId}`)
         console.log(req.users)
     }).put((req, res) => {
         res.send(`User Get id ${req.params.id}`)
-    }).delete((req, res) => {
+    }).delete((req, res) => {   
         res.send(`User Get id ${req.params.id}`)
     })
 
 
-const users = [{name : "Silvina"}, {name: "Alejandra"}]
 
-// this function will run everytime that finds a params that matchs the named passed
-router.param("id", (req, res, next, id) => {
-    req.users = users[id]
-    next() // If we don't add the function Next(), the code will continue running 
-    // we need to tell the programm to run the NEXT code
-})
+// // this function will run everytime that finds a params that matchs the named passed
+// router.param("id", (req, res, next, id) => {
+//     req.users = users[id]
+//     next() // If we don't add the function Next(), the code will continue running 
+//     // we need to tell the programm to run the NEXT code
+// })
 
 
 function logger(req, res, next){
--- server.js
+++ server.js
@@ -3,21 +3,30 @@ const app = express() // call express as a function to set up the server
 
 // name of the setting view engine, view engine: ejs
 app.set("view engine", "ejs")
+// express for static files 
+app.use(express.static("public"))
+
+// Middleware to access the data submited in a form
+app.use(express.urlencoded({extended: true}))
+
+// access json data and allow you to parse it from the body
+app.use(express.json())
+
 
 // app.use(logger)
 
-// create routes - Multiples functions can be passed on the app.method
-app.get("/",logger, (req, res) => {  // these are names conventions to request and response, but you can change them
-    console.log('Here')
-    // res.send('Hi') // send is used mostly for testing
-    // res.sendStatus(400) // this is displayed directly to the user with Internal Server Error (500)
-    // res.status(500).send('Hi') // this send a console error 500 and display Hi to the user
-    // res.status(500).json({message :" Error"}) // Console error and a json back to the user - Useful for API's
-    // res.download("server.js") // download a file!
+// // create routes - Multiples functions can be passed on the app.method
+// app.get("/", (req, res) => {  // these are names conventions to request and response, but you can change them
+//     console.log('Here')
+//     // res.send('Hi') // send is used mostly for testing
+//     // res.sendStatus(400) // this is displayed directly to the user with Internal Server Error (500)
+//     // res.status(500).send('Hi') // this send a console error 500 and display Hi to the user
+//     // res.status(500).json({message :" Error"}) // Console error and a json back to the user - Useful for API's
+//     // res.download("server.js") // download a file!
     
-    res.render("index", { text: "pulling text from server.js to index.html" })
+//     res.render("index", { text: "pulling text from server.js to index.html" })
 
-})
+// })
 
 
 
